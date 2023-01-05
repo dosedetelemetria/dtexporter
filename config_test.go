d34a8dda45d9269bc5250045cea425f2d556bbc5
Juraci Paixão Kröhling <juraci@kroehling.de>
2023-01-05 14:13:13 -0300

    adicionado teste que faz sentido
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- config_test.go
+++ config_test.go
@@ -4,12 +4,28 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
 )
 
 func TestLoadConfig(t *testing.T) {
+	// prepare
 	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
 	require.NoError(t, err)
 	require.NotNil(t, cm)
+
+	id := component.NewIDWithName("dtexporter", "2").String()
+	sub, err := cm.Sub(id)
+	require.NoError(t, err)
+
+	// test
+	cfg := &Config{}
+	err = component.UnmarshalConfig(sub, cfg)
+	require.NoError(t, err)
+
+	// verify
+	assert.Equal(t, "sp-1", cfg.DataCenter)
+	assert.Equal(t, "sp", cfg.Region)
 }
